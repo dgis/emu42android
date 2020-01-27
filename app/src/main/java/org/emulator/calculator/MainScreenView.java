40230c98ceac029b10635a70d88641e17f48e022
dgis <regis.cosnier@gmail.com>
2020-01-27 08:35:11 +0100

    * - Intercept the ESC keyboard key to allow the use of the BACK soft key.

--- app/src/main/java/org/emulator/calculator/MainScreenView.java
+++ app/src/main/java/org/emulator/calculator/MainScreenView.java
@@ -25,6 +25,7 @@ import android.graphics.Paint;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.SparseIntArray;
+import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
@@ -202,7 +203,8 @@ public class MainScreenView extends PanAndScaleView {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if((event.getFlags() & KeyEvent.FLAG_VIRTUAL_HARD_KEY) == 0) {
+        if((event.getFlags() & KeyEvent.FLAG_VIRTUAL_HARD_KEY) == 0
+        && (event.getSource() & InputDevice.SOURCE_KEYBOARD) == InputDevice.SOURCE_KEYBOARD) {
             char pressedKey = (char) event.getUnicodeChar();
             Integer windowsKeycode = charmap.get(pressedKey);
             if(windowsKeycode == null)
@@ -212,7 +214,7 @@ public class MainScreenView extends PanAndScaleView {
             if (windowsKeycode != 0)
                 NativeLib.keyDown(windowsKeycode);
             else if(debug) Log.e(TAG, String.format("Unknown keyCode: %d", keyCode));
-            if(keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_TAB)
+            if(keyCode == KeyEvent.KEYCODE_ESCAPE /*KEYCODE_BACK*/ || keyCode == KeyEvent.KEYCODE_TAB)
                 return true;
         }
         return super.onKeyDown(keyCode, event);
@@ -220,7 +222,8 @@ public class MainScreenView extends PanAndScaleView {
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if((event.getFlags() & KeyEvent.FLAG_VIRTUAL_HARD_KEY) == 0) {
+        if((event.getFlags() & KeyEvent.FLAG_VIRTUAL_HARD_KEY) == 0
+        && (event.getSource() & InputDevice.SOURCE_KEYBOARD) == InputDevice.SOURCE_KEYBOARD) {
             char pressedKey = (char) event.getUnicodeChar();
             Integer windowsKeycode = charmap.get(pressedKey);
             if(windowsKeycode == null)
@@ -230,7 +233,7 @@ public class MainScreenView extends PanAndScaleView {
             if (windowsKeycode != 0)
                 NativeLib.keyUp(windowsKeycode);
             else if(debug) Log.e(TAG, String.format("Unknown keyCode: %d", keyCode));
-            if(keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_TAB)
+            if(keyCode == KeyEvent.KEYCODE_ESCAPE /*KEYCODE_BACK*/ || keyCode == KeyEvent.KEYCODE_TAB)
                 return true;
         }
         return super.onKeyUp(keyCode, event);
